408b40c1506da267995b5d3f05698ebc7d7e8b2d
shubhamagr24 <shubhamagr24@gmil.com>
2025-05-11 04:08:16 +0530

    testing

--- testing.py
+++ testing.py
@@ -2,3 +2,5 @@ from toolkit import python_interpreter_tool,duckduckgo_search_tool
 
 print(python_interpreter_tool.invoke("print(1+1)"))
 
+print(duckduckgo_search_tool.invoke("python interpreter"))
+
