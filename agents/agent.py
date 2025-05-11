a034c23c93c3665557f31c93c6ec63851b914805
shubhamagr24 <shubhamagr24@gmil.com>
2025-05-11 23:40:04 +0530

    added agent and modified sys prompt

---
+++ agents/agent.py
@@ -0,0 +1,29 @@
+
+from langchain.agents import initialize_agent, AgentType
+from langchain_openai import ChatOpenAI
+from toolkit import tool_list  # List of Tool objects with structured input support
+from sample_ques import ques  # Import the question dictionary
+from system_prompt import system_prompt  # Import the system prompt
+
+
+# Initialize Chat Model
+llm = ChatOpenAI(model="gpt-4", temperature=0,max_tokens=2000)
+
+# Create the agent executor (handles multi-input tools via OpenAI Functions)
+agent_executor = initialize_agent(
+    tools=tool_list,
+    llm=llm,
+    agent=AgentType.OPENAI_FUNCTIONS,
+    verbose=True,
+    agent_kwargs={
+        "system_message": system_prompt,
+    },
+    max_iterations=7
+)
+
+# Run the agent
+response = agent_executor.invoke(
+    {"input": ques["Question"]}
+    )
+
+print(response['output'].split("FINAL ANSWER:")[-1].strip())
