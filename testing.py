e4c0b36fc064f19cf631148633530b3c8ead1041
shubhamagr24 <shubhamagr24@gmil.com>
2025-05-11 17:39:58 +0530

    testing

--- testing.py
+++ testing.py
@@ -3,4 +3,3 @@ from toolkit import python_interpreter_tool,duckduckgo_search_tool
 print(python_interpreter_tool.invoke("print(1+1)"))
 
 print(duckduckgo_search_tool.invoke("ind pak war fake news))
-
