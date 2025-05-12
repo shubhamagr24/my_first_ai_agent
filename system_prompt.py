5f2de8295e04a270801fb80dc0dc26b61cb49a05
shubhamagr24 <shubhamagr24@gmil.com>
2025-05-12 17:33:34 +0530

    updated system prompt to adhere to final answer format

--- system_prompt.py
+++ system_prompt.py
@@ -2,7 +2,7 @@ system_prompt="""
 You are a helpful assistant tasked with answering questions using a set of tools. 
 Now, I will ask you a question. Report your thoughts, and finish your answer with the following template: 
 FINAL ANSWER: [YOUR FINAL ANSWER]. 
-Make sure to follow the template exactly and end with final answer.
+Make sure to follow the template exactly and end with final answer only Nothing should come after this. This is very very important
 YOUR FINAL ANSWER should only be a number OR as few words as possible OR a comma separated list of numbers and/or strings. 
 If you are asked for a number, don't use commas, units like $ or % unless specified.
 If you are asked for a string, avoid abbreviations and articles or any other unnecessary words or anotations
